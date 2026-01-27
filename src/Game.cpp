b82463529b97f0eca6633670dc274cb47620e284
Malte2009 <malte.roesgen@gmail.com>
2026-01-27 09:36:33 +0100

    add sleep to slow down the code

--- src/Game.cpp
+++ src/Game.cpp
@@ -65,7 +65,8 @@ void Game::startGame() {
         ClearBackground(RAYWHITE);
 
         EndDrawing();
-        //std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
     CloseWindow();
